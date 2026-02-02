1e74dacaae3333edbb060046d1f619ade4bc625d
zzy-cl <zzy-cl@users.noreply.github.com>
2026-02-02 13:36:16 +0000

    deploy: e5443f8ad7a8d9ca8c52eee557f30262e6ffbdd9

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c95b4927.js',
+                path_to_root + 'searchindex-4a1a74fa.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
